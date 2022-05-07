de3988a0d7757145dec5628da2534926e022ecce
Italo Ferreira <grd788@gmail.com>
2022-05-07 14:26:54 -0300

    settings :gear: building and deploy backend

--- backend/src/modules/Feedback/useCases/SubmitFeedbackUseCase.ts
+++ backend/src/modules/Feedback/useCases/SubmitFeedbackUseCase.ts
@@ -16,15 +16,15 @@ export class SubmitFeedbackUseCase {
   async execute(request: SubmitFeedbackUseCaseRequest) {
     const { type, comment, screenshot } = request
 
-    if(!type) {
+    if (!type) {
       throw new Error('Type is required')
     }
 
-    if(!comment) {
+    if (!comment) {
       throw new Error('Comment is required')
     }
 
-    if(screenshot && !screenshot.startsWith('data:image/png;base64,')) {
+    if (screenshot && !screenshot.startsWith('data:image/png;base64,')) {
       throw new Error('Invalid screenshot format.')
     }
 
@@ -40,6 +40,7 @@ export class SubmitFeedbackUseCase {
         `<div style="font-family: sans-serif; font-size: 16px; color:#222">`,
         `<p>Tipo do Feedback ${type}</p>`,
         `<p>Comentário: ${comment}</p>`,
+        screenshot ? `<img src="${screenshot}"/>` : ``,
         `<div/>`
       ].join('\n')
     })
