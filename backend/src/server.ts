a9c5b7f52ae570fd8fcb086dba0448524dadb853
Italo Ferreira <grd788@gmail.com>
2022-05-07 17:51:28 -0300

    settings :gear: railway config port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,6 +8,6 @@ app.use(express.json())
 app.use(cors())
 app.use(routes)
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('Server rodando na porta 3333!')
 })
